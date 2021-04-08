a1136abfd19114dbac08428b498d49558ea8b912
spenalva <penalvasantiagogm@gmail.com>
2021-04-08 18:31:20 -0300

    keystone started

---
+++ backend/keystone.ts
@@ -0,0 +1,32 @@
+import { config, createSchema } from '@keystone-next/keystone/schema';
+import 'dotenv/config';
+
+const databaseURL =
+  process.env.DATABASE_URL || 'mongodb://localhost/keystone-sick-fits-tutorial';
+
+const sessionConfig = {
+  maxAge: 60 * 60 * 24 * 60,
+  secret: process.env.COOKIE_SECRET,
+};
+
+export default config({
+  server: {
+    cors: {
+      origin: [process.env.FRONTEND_URL],
+      credentials: true,
+    },
+  },
+  db: {
+    adapter: 'mongoose',
+    url: databaseURL,
+    // TODO add data seeing here
+  },
+  lists: createSchema({
+    // Schema items go in here
+  }),
+  ui: {
+    // TODO change this for roles,
+    isAccessAllowed: () => true,
+  },
+  // TODO add session values here
+});
